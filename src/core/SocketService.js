c91bc2b4f2133b57ac05448b6f02db33c7f827e0
sirromdev <sirromdev@gmail.com>
2019-02-01 14:02:52 +0000

    added socket service

--- src/core/SocketService.js
+++ src/core/SocketService.js
@@ -1,9 +1,56 @@
 import { Socket } from 'phoenix';
 
 export default class SocketService {
-  constructor(url = 'ws://127.1:4000/socket') {
-    this._url = url;
+  constructor() {
+    this._url = null;
+    this._socket = null;
   }
 
-  init() {}
+  init(url = 'ws://127.1:4000/socket') {
+    this._url = url;
+    return this.connect();
+  }
+
+  connect() {
+    return new Promise((resolve, reject) => {
+      this._socket = new Socket(this._url, {
+        transport: WebSocket
+      });
+
+      this._socket.onOpen(() => {
+        resolve();
+      });
+
+      this._socket.onError(e => {
+        reject('Socket Failed To Connect');
+      });
+
+      this._socket.connect();
+    });
+  }
+
+  disconnect() {
+    return new Promise(resolve => {
+      this._socket.disconnect(resolve);
+    });
+  }
+
+  url() {
+    return this._url;
+  }
+
+  socket() {
+    return this._socket;
+  }
+
+  connected() {
+    if (this.socket()) {
+      return this.socket().isConnected();
+    }
+    return this._socket.isConnected();
+  }
+
+  channel(...args) {
+    return this.socket.channel(...args);
+  }
 }
---
+++ test/core/SocketService.test.js
@@ -0,0 +1,31 @@
+import SocketService from '../../src/core/SocketService.js';
+
+let service;
+
+beforeEach(() => {
+  service = new SocketService();
+});
+
+test('service will initialise correctly', async () => {
+  await service.init();
+  expect(service.connected()).toBe(true);
+});
+
+test('service will fail to initialise with incorrect url', async () => {
+  expect.assertions(2);
+  const incorrectUrl = 'ws://a.a.a.a:4000/socket';
+
+  try {
+    await service.init(incorrectUrl);
+  } catch (e) {
+    expect(e).toEqual('Socket Failed To Connect');
+  }
+  expect(service.connected()).toBe(false);
+});
+
+test('service will disconnect', async () => {
+  await service.init();
+  expect(service.connected()).toBe(true);
+  await service.disconnect();
+  expect(service.connected()).toBe(false);
+});
