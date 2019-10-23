880c02cbd056e9ab97656db1d8167541e8a25a77
Phil Bain <bain.pmcg@gmail.com>
2019-10-23 14:13:10 +0200

    consolidate tests to use a single testchain instance

--- test/Client.test.js
+++ test/Client.test.js
@@ -26,214 +26,350 @@ const stackPayload = {
       type: chainType,
       accounts: 2,
       block_mine_time: 0,
-      clean_on_stop: true
+      clean_on_stop: false
     },
     deps: []
   }
 };
 
-const _stop = id => {
-  client.stop(id);
-  return client.sequenceEvents(id, [OK, TERMINATED]);
-};
+describe('Basic testchain functions', async () => {
+  const _stop = id => {
+    client.stop(id);
+    return client.sequenceEvents(id, [OK, TERMINATED]);
+  };
 
-const _restart = id => {
-  client.restart(id);
-  return client.sequenceEvents(id, [READY]);
-};
+  const _restart = id => {
+    client.restart(id);
+    return client.sequenceEvents(id, [READY]);
+  };
 
-const _takeSnapshot = (id, description) => {
-  client.takeSnapshot(id, description);
-  return client.sequenceEvents(id, [
-    Event.TAKING_SNAPSHOT,
-    Event.SNAPSHOT_TAKEN,
-    Event.ACTIVE
-  ]);
-};
+  const _takeSnapshot = (id, description) => {
+    client.takeSnapshot(id, description);
+    return client.sequenceEvents(id, [
+      Event.TAKING_SNAPSHOT,
+      Event.SNAPSHOT_TAKEN,
+      Event.ACTIVE
+    ]);
+  };
 
-const _restoreSnapshot = async (id, snapshot) => {
-  client.restoreSnapshot(id, snapshot);
-  return client.sequenceEvents(id, [
-    Event.REVERTING_SNAPSHOT,
-    Event.SNAPSHOT_REVERTED,
-    Event.ACTIVE
-  ]);
-};
+  const _restoreSnapshot = async (id, snapshot) => {
+    client.restoreSnapshot(id, snapshot);
+    return client.sequenceEvents(id, [
+      Event.REVERTING_SNAPSHOT,
+      Event.SNAPSHOT_REVERTED,
+      Event.ACTIVE
+    ]);
+  };
 
-beforeEach(() => {
-  // Setting inordinately high until we figure out threshhold for circleci
-  jest.setTimeout(120000);
-  client = new Client(testchainUrl, websocketUrl);
-});
+  beforeAll(() => {
+    // Setting inordinately high until we figure out threshhold for circleci
+    // jest.setTimeout(120000);
+    client = new Client(testchainUrl, websocketUrl);
+  });
 
-afterEach(async () => {
-  await sleep(10000);
-  const { data: list } = await client.api.listAllChains();
-  for (const chain of list) {
-    const { id } = chain;
-    await client.delete(id);
-  }
-
-  await sleep(10000);
-
-  for (const type of ['ganache', 'geth']) {
-    const { data: snapshots } = await client.api.listAllSnapshots(type);
-    for (const { id } of snapshots) {
-      await client.api.deleteSnapshot(id);
+  afterAll(async () => {
+    // await sleep(10000);
+    const { data: list } = await client.api.listAllChains();
+    for (const chain of list) {
+      const { id } = chain;
+      await client.delete(id);
     }
-  }
 
-  await sleep(10000);
+    await sleep(10000);
+
+    for (const type of ['ganache', 'geth']) {
+      const { data: snapshots } = await client.api.listAllSnapshots(type);
+      for (const { id } of snapshots) {
+        await client.api.deleteSnapshot(id);
+      }
+    }
+
+    await sleep(10000);
+  });
+
+  let testchainId1;
+
+  test('client will be created correctly', () => {
+    expect(client).toBeInstanceOf(Client);
+    expect(client.api).toBeInstanceOf(Api);
+    expect(client.socket).toBeInstanceOf(SocketHandler);
+  });
+
+  test('client will initialise socket connection', async () => {
+    expect(client.socket.connected).toBe(false);
+    await client.init();
+    expect(client.socket.connected).toBe(true);
+    expect(client.connections[0]).toEqual(API);
+    expect(client.channel(API).joined).toBe(true);
+  });
+
+  test('startStack method will start a geth testchain stack in a READY state', async () => {
+    const {
+      data: { id: expectedId }
+    } = await client.api.startStack(stackPayload);
+    await client.sequenceEvents(expectedId, [OK, READY]);
+
+    // Use this ID for the rest of the tests in this block.
+    testchainId1 = expectedId;
+
+    const {
+      details: { status, id }
+    } = await client.api.getChain(expectedId);
+
+    expect(status).toEqual(READY);
+    expect(id).toEqual(expectedId);
+  }, 10000);
+
+  test('client will stop a chain instance', async () => {
+    const eventData = await _stop(testchainId1);
+
+    expect(Object.keys(eventData)).toEqual([OK, TERMINATED]);
+
+    const {
+      details: { status }
+    } = await client.api.getChain(testchainId1);
+
+    expect(status).toEqual(TERMINATED);
+  }, 25000);
+
+  test('client will restart a stopped chain', async () => {
+    const {
+      details: { status: status1 }
+    } = await client.api.getChain(testchainId1);
+
+    expect(status1).toEqual(TERMINATED);
+
+    const eventData = await _restart(testchainId1);
+
+    expect(Object.keys(eventData)).toEqual([READY]);
+
+    const {
+      details: { status: status2 }
+    } = await client.api.getChain(testchainId1);
+
+    expect(status2).toEqual(READY);
+  }, 20000);
+
+  test('client will take a snapshot of chain started with "clean_on_stop: false"', async () => {
+    // Note: Taking a snapshot will stop the chain, so config option "clean_on_stop" must be false.
+
+    const timestamp = new Date();
+    const snapshotDescription = `Jest takeSnapshot ${timestamp.toUTCString()}`;
+    const eventData = await _takeSnapshot(testchainId1, snapshotDescription);
+
+    expect(Object.keys(eventData)).toEqual([
+      Event.TAKING_SNAPSHOT,
+      Event.SNAPSHOT_TAKEN,
+      Event.ACTIVE
+    ]);
+
+    const { snapshot_taken } = eventData;
+    const { id: snapId } = snapshot_taken;
+    const { data: list } = await client.api.listAllSnapshots(chainType);
+    const snapshot = list.find(snapshot => snapshot.id === snapId);
+
+    expect(snapshot.description).toBe(snapshotDescription);
+
+    await sleep(1000);
+  }, 25000);
+
+  test('client will restore a snapshot', async () => {
+    // Note: Taking a snapshot will stop the chain, so config option "clean_on_stop" must be false.
+
+    const timestamp = new Date();
+    const snapshotDescription = `Jest restoreSnapshot ${timestamp.toUTCString()}`;
+    const {
+      snapshot_taken: { id: snapshotId }
+    } = await _takeSnapshot(testchainId1, snapshotDescription);
+
+    // must wait for chain to move to status: active before trying to restore snapshot
+    await sleep(1000);
+
+    const eventData = await _restoreSnapshot(testchainId1, snapshotId);
+
+    expect(Object.keys(eventData)).toEqual([
+      Event.REVERTING_SNAPSHOT,
+      Event.SNAPSHOT_REVERTED,
+      Event.ACTIVE
+    ]);
+
+    const { snapshot_reverted } = eventData;
+    const { id: snapId } = snapshot_reverted;
+    const { data: list } = await client.api.listAllSnapshots(chainType);
+    const snapshot = list.find(snapshot => snapshot.id === snapId);
+
+    expect(snapshot.description).toBe(snapshotDescription);
+
+    await sleep(1000);
+  }, 30000);
+
+  test('client will delete a chain', async () => {
+    const { data: list1 } = await client.api.listAllChains();
+
+    expect(list1.find(chain => chain.id === testchainId1)).toBeDefined();
+
+    await client.delete(testchainId1);
+
+    const { data: list2 } = await client.api.listAllChains();
+
+    expect(list2.find(chain => chain.id === testchainId1)).not.toBeDefined();
+  }, 60000);
 });
 
-test.only('client will be created correctly', () => {
-  expect(client).toBeInstanceOf(Client);
-  expect(client.socket).toBeInstanceOf(SocketHandler);
-});
+// // test.only('API will be created correctly', () => {
+// //   expect(client.api).toBeInstanceOf(Api);
+// // });
 
-test.only('client will initialise socket connection', async () => {
-  expect(client.socket.connected).toBe(false);
-  await client.init();
-  expect(client.socket.connected).toBe(true);
-  expect(client.connections[0]).toEqual(API);
-  expect(client.channel(API).joined).toBe(true);
-});
+// // test('startStack method will start a geth testchain stack in a READY state', async () => {
+// //   await client.init();
+// //   const {
+// //     data: { id: expectedId }
+// //   } = await client.api.startStack(stackPayload);
+// //   await client.sequenceEvents(expectedId, [OK, READY]);
 
-test.only('client will stop a chain instance', async () => {
-  await client.init();
-  const {
-    data: { id }
-  } = await client.api.startStack(stackPayload);
-  await client.sequenceEvents(id, [OK, READY]);
+// //   const {
+// //     details: { status, id }
+// //   } = await client.api.getChain(expectedId);
 
-  const eventData = await _stop(id);
+// //   expect(status).toEqual(READY);
+// //   expect(id).toEqual(expectedId);
+// // }, 10000);
 
-  expect(Object.keys(eventData)).toEqual([OK, TERMINATED]);
+// // TODO: Finish test implementation when endpoint is fixed.
+// xtest('getStackInfo will return an object with stack information', async () => {
+//   await client.init();
+//   const {
+//     data: { id }
+//   } = await client.api.startStack(stackPayload);
+//   await client.sequenceEvents(id, [OK, READY]);
 
-  const {
-    details: { status }
-  } = await client.api.getChain(id);
+//   const stackInfo = await client.api.getStackInfo(id);
+//   console.log('stackInfo', stackInfo);
+// });
 
-  expect(status).toEqual(TERMINATED);
-}, 25000);
+// test('getChain will return an object with correct chain details', async () => {
+//   await client.init();
+//   const {
+//     data: { id }
+//   } = await client.api.startStack(stackPayload);
+//   await client.sequenceEvents(id, [OK, READY]);
 
-xtest('client will restart a stopped chain', async () => {
-  await client.init();
-  const {
-    data: { id }
-  } = await client.api.startStack(stackPayload);
-  await client.sequenceEvents(id, [OK, READY]);
+//   const { details: chainData } = await client.api.getChain(id);
+//   const { config, chain_details: chainDetails } = chainData;
+//   const chainDataKeys = Object.keys(chainData);
+//   const configKeys = Object.keys(config);
+//   const chainDetailsKeys = Object.keys(chainDetails);
 
-  await _stop(id);
+//   expect(chainDataKeys).toEqual([
+//     'status',
+//     'id',
+//     'deploy_step',
+//     'deploy_hash',
+//     'deploy_data',
+//     'config',
+//     'chain_details'
+//   ]);
 
-  await sleep(10000);
+//   expect(configKeys).toEqual([
+//     'type',
+//     'step_id',
+//     'snapshot_id',
+//     'node',
+//     'network_id',
+//     'id',
+//     'description',
+//     'deploy_tag',
+//     'clean_on_stop',
+//     'block_mine_time',
+//     'accounts'
+//   ]);
 
-  const {
-    details: { status: status1 }
-  } = await client.api.getChain(id);
+//   expect(chainDetailsKeys).toEqual([
+//     'ws_url',
+//     'rpc_url',
+//     'network_id',
+//     'id',
+//     'gas_limit',
+//     'coinbase',
+//     'accounts'
+//   ]);
+// });
 
-  expect(status1).toEqual(TERMINATED);
+// test('listAllChains will return an array of existing chains', async () => {
+//   await client.init();
+//   const {
+//     data: { id }
+//   } = await client.api.startStack(stackPayload);
+//   await client.sequenceEvents(id, [OK, READY]);
 
-  const eventData = await _restart(id);
-  expect(Object.keys(eventData)).toEqual([READY]);
+//   const { data } = await client.api.listAllChains(id);
 
-  const {
-    details: { status: status2 }
-  } = await client.api.getChain(id);
-  expect(status2).toEqual(READY);
-}, 20000);
+//   expect(Array.isArray(data)).toBe(true);
+//   expect(data.length).toBe(1);
+// });
 
-test('client will delete a chain', async () => {
-  await client.init();
-  const {
-    data: { id: id1 }
-  } = await client.api.startStack(stackPayload);
-  await client.sequenceEvents(id1, [OK, READY]);
+// // test(
+// //   'client will create a chain instance with deployments',
+// //   async () => {
+// //     await client.init();
 
-  const {
-    data: { id: id2 }
-  } = await client.api.startStack(stackPayload);
-  await client.sequenceEvents(id2, [OK, READY]);
+// //     const eventData = await _create({ ...options, scenario_id: 1 });
+// //     expect(Object.keys(eventData)).toEqual([
+// //       Event.OK,
+// //       Event.DEPLOYING,
+// //       Event.DEPLOYED,
+// //       Event.READY,
+// //       Event.ACTIVE
+// //     ]);
 
-  const { data: list1 } = await client.api.listAllChains();
-  expect(list1.find(chain => chain.id === id1)).toBeDefined();
-  expect(list1.find(chain => chain.id === id2)).toBeDefined();
+// //     const { ready } = eventData;
+// //     const { id } = ready;
+// //     const { details } = await client.api.getChain(id);
 
-  await client.delete(id1);
-  await client.delete(id2);
+// //     const { chain_details, deploy_step, deploy_hash } = details;
 
-  const { data: list2 } = await client.api.listAllChains();
-  expect(list2.find(chain => chain.id === id1)).not.toBeDefined();
-  expect(list2.find(chain => chain.id === id2)).not.toBeDefined();
-}, 60000);
-
-test('client will take a snapshot of chain started with "clean_on_stop: false"', async () => {
-  // Taking a snapshot will stop the chain, so clean_on_stop must be false.
-  const modifiedPayload = { ...stackPayload };
-  modifiedPayload.testchain.config.clean_on_stop = false;
-
-  await client.init();
-  const {
-    data: { id: id }
-  } = await client.api.startStack(modifiedPayload);
-  await client.sequenceEvents(id, [OK, READY]);
-
-  const timestamp = new Date();
-
-  const snapshotDescription = `Jest takeSnapshot ${timestamp.toUTCString()}`;
-  const eventData = await _takeSnapshot(id, snapshotDescription);
-
-  expect(Object.keys(eventData)).toEqual([
-    Event.TAKING_SNAPSHOT,
-    Event.SNAPSHOT_TAKEN,
-    Event.ACTIVE
-  ]);
-
-  const { snapshot_taken } = eventData;
-  const { id: snapId } = snapshot_taken;
-  const { data: list } = await client.api.listAllSnapshots(chainType);
-  const snapshot = list.find(snapshot => snapshot.id === snapId);
-
-  expect(snapshot.description).toBe(snapshotDescription);
-
-  await sleep(1000);
-}, 25000);
-
-test('client will restore a snapshot', async () => {
-  // Taking a snapshot will stop the chain, so clean_on_stop must be false.
-  const modifiedPayload = { ...stackPayload };
-  modifiedPayload.testchain.config.clean_on_stop = false;
-
-  await client.init();
-  const {
-    data: { id: id }
-  } = await client.api.startStack(modifiedPayload);
-
-  await client.sequenceEvents(id, [OK, READY]);
-
-  const timestamp = new Date();
-  const snapshotDescription = `Jest restoreSnapshot ${timestamp.toUTCString()}`;
-  const {
-    snapshot_taken: { id: snapshotId }
-  } = await _takeSnapshot(id, snapshotDescription);
-
-  // must wait for chain to move to status: active before trying to restore snapshot
-  await sleep(1000);
-
-  const eventData = await _restoreSnapshot(id, snapshotId);
-
-  expect(Object.keys(eventData)).toEqual([
-    Event.REVERTING_SNAPSHOT,
-    Event.SNAPSHOT_REVERTED,
-    Event.ACTIVE
-  ]);
-
-  const { snapshot_reverted } = eventData;
-  const { id: snapId } = snapshot_reverted;
-  const { data: list } = await client.api.listAllSnapshots(chainType);
-  const snapshot = list.find(snapshot => snapshot.id === snapId);
-
-  expect(snapshot.description).toBe(snapshotDescription);
-
-  await sleep(1000);
-}, 30000);
+// //     const { deployed } = eventData;
+// //     expect(Object.keys(deployed)).toEqual([
+// //       'MCD_JUG',
+// //       'PROXY_ACTIONS',
+// //       'MCD_VAT',
+// //       'MCD_JOIN_REP',
+// //       'MCD_SPOT',
+// //       'MCD_DAI',
+// //       'MCD_MOM_LIB',
+// //       'CDP_MANAGER',
+// //       'MCD_PIT',
+// //       'MCD_FLOP',
+// //       'VAL_REP',
+// //       'MCD_DEPLOY',
+// //       'MCD_FLAP',
+// //       'PIP_REP',
+// //       'MCD_FLIP_REP',
+// //       'VOTE_PROXY_FACTORY',
+// //       'PROXY_REGISTRY',
+// //       'PROXY_FACTORY',
+// //       'MCD_MOVE_DAI',
+// //       'MCD_GOV',
+// //       'REP',
+// //       'MCD_JOIN_DAI',
+// //       'PIP_ETH',
+// //       'MCD_ADM',
+// //       'MCD_MOM',
+// //       'MCD_FLIP_ETH',
+// //       'MCD_MOVE_ETH',
+// //       'MCD_CAT',
+// //       'MCD_POT',
+// //       'VAL_ETH',
+// //       'MCD_VOW',
+// //       'MCD_JOIN_ETH',
+// //       'MCD_MOVE_REP',
+// //       'MCD_GOV_GUARD',
+// //       'MCD_DAI_GUARD',
+// //       'MULTICALL'
+// //     ]);
+// //     expect(deploy_hash).toBeDefined();
+// //     expect(deploy_step.description).toEqual('Scenario 1 - General deployment');
+// //     expect(isEqual(chain_details, ready)).toBe(true);
+// //   },
+// //   4 * 60 * 1000
+// // ); // this test does take 2.5 - 3 minutes
